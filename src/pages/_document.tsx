0c73e82b06e6038123a11cb3bbd711772eb5d698
fatihfahroji10@gmail.com <fatihfahroji10@gmail.com>
2024-01-12 10:17:27 +0700

    update _document head to include manifest

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,12 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="Simple Blog App build with NextJS" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/images/icons/favicon.png" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
